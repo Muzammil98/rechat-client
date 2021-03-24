7a2b92cf340bcca04d3b16292ed18a50e3a16a4d
Muzammil98 <muzammilsarwar0@gmail.com>
2021-03-24 10:59:38 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
 function App() {
-  const [msgs, setMsgs] = useState([])
+  const [text, setText] = useState("");
+  const [name, setName] = useState("");
+  const [msgs, setMsgs] = useState([]);
   const socket = io("localhost:4000");
 
-  socket.on('message',(message)=>{
-   setMsgs((prev) => [...prev,message])
-    console.log(msgs)
+  useEffect(() => {
+    const user = prompt("Your name?");
+    appendMessage("You joined");
+    setName(user);
+  }, []);
 
-  })
+  useEffect(() => {
+    socket.emit("new-user", name);
+    return () => {
+      socket.close();
+    };
+  }, [name]);
+ 
+  useEffect(() => {
+    socket.on("chat-message", (data) => {
+      console.log('DTA',data)
+      appendMessage(`${data.name}: ${data.message}`);
+    });
+    socket.on("user-connected", (name) => {
+      appendMessage(`${name} connected`);
+    });
+  
+    return () => {
+     
+      socket.close();
+     
+    };
+  }, [socket]);
 
+  const appendMessage = (message) => {
+    setMsgs((prev) => [...prev, message]);
+  };
+  
+ 
   const msgEmit = () => {
-      socket.emit('message','HI')
-  }
+    const d = new Date();
+    const time = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+    const msg = text + " " + time;
+    appendMessage(`You: ${msg}`);
+
+    socket.emit("send-chat-message", msg);
+    setText("");
+  };
   return (
     <div>
-      Hello world
-      <button onClick={()=>msgEmit()}>Send</button>
-      <code>{JSON.stringify(msgs)}</code>
+      <h2>Send a hi message</h2>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={() => msgEmit()}>Send</button>
+      
+      <code>
+        <ul>
+          {msgs.map((msg, index) => (
+            <li key={index}>{msg.toString()}</li>
+          ))}
+        </ul>
+      </code>
     </div>
   );
 }
